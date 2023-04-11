d8f3553f9d6a884186e921050fe760bc73b78284
Jordan Fontaine <jordanmfontaine@gmail.com>
2023-04-11 09:02:12 -0400

    Addressed bug in middleware auth file

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,15 +9,13 @@ export const verifyToken = async (req, res, next) => {
     }
 
     if (token.startsWith("Bearer ")) {
-      token = token.slice(7, token.length).trimLeft;
+      token = token.slice(7, token.length).trimLeft();
     }
 
     const verified = jwt.verify(token, process.env.JWT_SECRET);
     req.user = verified;
     next();
-    
   } catch (err) {
-  
     res.status(500).json({ err: err.message });
   }
 };
